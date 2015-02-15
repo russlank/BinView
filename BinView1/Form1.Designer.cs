b155972c603cb62cfd3a811feeea3a7982e2df6f
Russlan <russlank@gmail.com>
2015-02-15 21:50:08 +0100

    first commit

---
+++ BinView1/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace BinView1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.NormalizeChk = new System.Windows.Forms.CheckBox();
+            this.ScaleChk = new System.Windows.Forms.CheckBox();
+            this.OpenBtn = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.DataPictureBox = new System.Windows.Forms.PictureBox();
+            this.openFileDlg = new System.Windows.Forms.OpenFileDialog();
+            this.FullViewChk = new System.Windows.Forms.CheckBox();
+            this.BlockSizeBox = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.BlockSizeBox);
+            this.panel1.Controls.Add(this.FullViewChk);
+            this.panel1.Controls.Add(this.NormalizeChk);
+            this.panel1.Controls.Add(this.ScaleChk);
+            this.panel1.Controls.Add(this.OpenBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(697, 30);
+            this.panel1.TabIndex = 0;
+            // 
+            // NormalizeChk
+            // 
+            this.NormalizeChk.AutoSize = true;
+            this.NormalizeChk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.NormalizeChk.Location = new System.Drawing.Point(183, 7);
+            this.NormalizeChk.Name = "NormalizeChk";
+            this.NormalizeChk.Size = new System.Drawing.Size(69, 17);
+            this.NormalizeChk.TabIndex = 2;
+            this.NormalizeChk.Text = "Normalize";
+            this.NormalizeChk.UseVisualStyleBackColor = true;
+            this.NormalizeChk.CheckedChanged += new System.EventHandler(this.NormalizeChk_CheckedChanged);
+            // 
+            // ScaleChk
+            // 
+            this.ScaleChk.AutoSize = true;
+            this.ScaleChk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ScaleChk.Location = new System.Drawing.Point(96, 7);
+            this.ScaleChk.Name = "ScaleChk";
+            this.ScaleChk.Size = new System.Drawing.Size(48, 17);
+            this.ScaleChk.TabIndex = 1;
+            this.ScaleChk.Text = "Scale";
+            this.ScaleChk.UseVisualStyleBackColor = true;
+            this.ScaleChk.CheckedChanged += new System.EventHandler(this.Scale_CheckedChanged);
+            // 
+            // OpenBtn
+            // 
+            this.OpenBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OpenBtn.Location = new System.Drawing.Point(3, 3);
+            this.OpenBtn.Name = "OpenBtn";
+            this.OpenBtn.Size = new System.Drawing.Size(75, 23);
+            this.OpenBtn.TabIndex = 0;
+            this.OpenBtn.Text = "Open File";
+            this.OpenBtn.UseVisualStyleBackColor = true;
+            this.OpenBtn.Click += new System.EventHandler(this.OpenBtn_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 30);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(697, 472);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Black;
+            this.panel4.Controls.Add(this.pictureBox1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(128, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(569, 472);
+            this.panel4.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(569, 472);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Navy;
+            this.panel3.Controls.Add(this.DataPictureBox);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(128, 472);
+            this.panel3.TabIndex = 0;
+            // 
+            // DataPictureBox
+            // 
+            this.DataPictureBox.BackColor = System.Drawing.Color.MidnightBlue;
+            this.DataPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DataPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.DataPictureBox.Name = "DataPictureBox";
+            this.DataPictureBox.Size = new System.Drawing.Size(128, 472);
+            this.DataPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.DataPictureBox.TabIndex = 0;
+            this.DataPictureBox.TabStop = false;
+            this.DataPictureBox.Click += new System.EventHandler(this.DataPictureBox_Click);
+            this.DataPictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DataPictureBox_MouseDown);
+            this.DataPictureBox.MouseHover += new System.EventHandler(this.DataPictureBox_MouseHover);
+            this.DataPictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.DataPictureBox_MouseMove);
+            // 
+            // openFileDlg
+            // 
+            this.openFileDlg.FileName = "*.*";
+            this.openFileDlg.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // FullViewChk
+            // 
+            this.FullViewChk.AutoSize = true;
+            this.FullViewChk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.FullViewChk.Location = new System.Drawing.Point(277, 7);
+            this.FullViewChk.Name = "FullViewChk";
+            this.FullViewChk.Size = new System.Drawing.Size(64, 17);
+            this.FullViewChk.TabIndex = 3;
+            this.FullViewChk.Text = "Full View";
+            this.FullViewChk.UseVisualStyleBackColor = true;
+            this.FullViewChk.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // BlockSizeBox
+            // 
+            this.BlockSizeBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BlockSizeBox.Location = new System.Drawing.Point(374, 4);
+            this.BlockSizeBox.Name = "BlockSizeBox";
+            this.BlockSizeBox.Size = new System.Drawing.Size(100, 20);
+            this.BlockSizeBox.TabIndex = 4;
+            this.BlockSizeBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.BlockSizeBox.Leave += new System.EventHandler(this.BlockSizeBox_Leave);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 502);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.Text = "BinView";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DataPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button OpenBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDlg;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox ScaleChk;
+        private System.Windows.Forms.CheckBox NormalizeChk;
+        private System.Windows.Forms.PictureBox DataPictureBox;
+        private System.Windows.Forms.CheckBox FullViewChk;
+        private System.Windows.Forms.TextBox BlockSizeBox;
+    }
+}
+
---
+++ BinView1/Form1.cs
@@ -0,0 +1,415 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Drawing2D;
+
+namespace BinView1
+{
+    public partial class MainForm : Form
+    {
+        byte[] m_data;
+        Int32[,] m_finger_print = new Int32[256, 256];
+        Int32 m_max_dencity = 0;
+        Int32 m_avarage_dencity = 0;
+
+        int m_block_size = 10240;
+        int m_previous_y = -1;
+        
+        public MainForm()
+        {
+            InitializeComponent();
+
+            m_data = null;
+
+            BlockSizeBox.Text = m_block_size.ToString();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            
+
+        }
+
+        private void CalculateRangeFingerPrint(int range_start, int range_end)
+        {
+            if (m_data != null)
+            {
+                int x, y;
+                int start;
+                int end;
+
+                m_max_dencity = 0;
+                m_avarage_dencity = 0;
+
+                for (x = 0; x < 256; x++)
+                    for (y = 0; y < 256; y++)
+                        m_finger_print[x, y] = 0;
+
+                if (range_start < range_end)
+                {
+                    start = range_start;
+                    end = range_end;
+                } else {
+                    start = range_end;
+                    end = range_start;
+                }
+
+                if (start < 0)
+                {
+                    start = 0;
+                    if (end < 0) end = 0;
+                }
+
+                if (end >= m_data.Length)
+                {
+                    end = m_data.Length - 1;
+                    if (start >= m_data.Length - 1) start  = m_data.Length - 1;
+                }
+
+                y = m_data[start];
+
+                for (int i = start + 1; i < end; i++)
+                {
+                    x = y;
+                    y = m_data[i];
+                    m_finger_print[x, y]++;
+
+                    if (m_max_dencity < m_finger_print[x, y]) m_max_dencity = m_finger_print[x, y];
+                }
+
+                for (x = 0; x < 256; x++)
+                    for (y = 0; y < 256; y++)
+                        m_avarage_dencity += m_finger_print[x, y];
+
+                m_avarage_dencity = m_avarage_dencity / (256 * 256);
+
+            }
+        }
+
+        
+        private void CalculateFingerPrint()
+        {
+            if (m_data != null)
+            {
+                int x, y;
+
+                m_max_dencity = 0;
+                m_avarage_dencity = 0;
+
+                for (x = 0; x < 256; x++)
+                    for (y = 0; y < 256; y++)
+                        m_finger_print[x, y] = 0;
+
+                y = m_data[0];
+
+                for (int i = 1; i < m_data.Length; i++)
+                {
+                    x = y;
+                    y = m_data[i];
+                    m_finger_print[x, y]++;
+
+                    if (m_max_dencity < m_finger_print[x, y]) m_max_dencity = m_finger_print[x, y];
+                }
+
+                for (x = 0; x < 256; x++)
+                    for (y = 0; y < 256; y++)
+                        m_avarage_dencity += m_finger_print[x, y];
+
+                m_avarage_dencity = m_avarage_dencity / (256 * 256);
+
+            }
+        }
+
+        private void DrawDataImage()
+        {
+            const int Width = 128;
+            int Height = (int)Math.Ceiling((float)(m_data.Length) / (float)(Width));
+
+            Bitmap bm = new Bitmap(Width, Height);
+            for (int i = 1; i < m_data.Length; i++)
+            {
+                bm.SetPixel(i % Width, i / Width, Color.FromArgb(255, 32, m_data[i], 64));
+
+            }
+
+            DataPictureBox.Image = bm;
+        }
+
+        private void DrawFingerPrint()
+        {
+            //Bitmap bm = new Bitmap( 512, 512);
+            Bitmap bm = new Bitmap(256, 256);
+
+            bool Scaled = ScaleChk.Checked;
+            bool Normalize = NormalizeChk.Checked;
+
+            int x, y;
+
+            if (Scaled)
+            {
+                if (Normalize)
+                {
+                    for( x = 0; x < 256; x++) for (y = 0; y < 256; y++)
+                    {
+                        Int32 dencity = m_finger_print[x,y];
+                        byte v_dencity;
+
+                        if (dencity < m_avarage_dencity)
+                        {
+                            v_dencity = (byte)(((float)180.0 * (float)dencity) / (float)m_avarage_dencity);
+                        } else {
+                            v_dencity = (byte)(180.0 + ((float)75.0 * (float)dencity) / (float)m_max_dencity);
+                        }
+                        
+                        
+                        Color co = Color.FromArgb(255, v_dencity, v_dencity, v_dencity);
+
+
+                        if (m_finger_print[x, y] > 0)
+                        {
+                            bm.SetPixel(x, y, co);
+                        }
+                    }                    
+
+                } else {
+                    
+                    for( x = 0; x < 256; x++) for (y = 0; y < 256; y++)
+                    {
+                        Int32 dencity = m_finger_print[x,y];
+                        byte v_dencity;
+
+                        if (dencity < 256) v_dencity = (byte)dencity;
+                        else v_dencity = 255;
+
+                        Color co = Color.FromArgb(255, v_dencity, v_dencity, v_dencity);
+
+                        if (m_finger_print[x, y] > 0)
+                        {
+                            bm.SetPixel(x, y, co);
+                        }
+                    }                    
+                }
+
+            } else {
+                Color active = Color.FromArgb(255, 255, 255, 255);
+                //Color iniactive = Color.FromArgb(255, 255, 255, 255);
+
+                for( x = 0; x < 256; x++) for (y = 0; y < 256; y++)
+                {
+                    if (m_finger_print[x, y] > 0)
+                    {
+                        bm.SetPixel(x, y, active);
+                    }
+                }
+            }
+
+            pictureBox1.Image = bm;
+        }
+
+        /*
+
+        private void DrawData()
+        {
+            Bitmap bm = new Bitmap( 512, 512);
+            bool Scaled = ScaleChk.Checked;
+            bool Normalize = NormalizeChk.Checked;
+
+            if (m_data != null)
+            {
+                int x;
+                int y;
+                int Max = 0;
+
+                for (x = 0; x < 512; x++)
+                    for (y = 0; y < 512; y++)
+                    {
+                        bm.SetPixel(x, y, Color.FromArgb(255,0,0,0));
+                    }
+
+                
+
+                if (Scaled)
+                {
+                    y = m_data[0] * 2;
+                    for (int i = 1; i < m_data.Length; i++)
+                    {
+                        x = y;
+                        y = m_data[i] * 2;
+                        Color c = bm.GetPixel(x, y);
+                        Color nc;
+
+                        int Current = c.R;
+
+                        if (Current < 255) Current = Current + 1;
+
+                        nc = Color.FromArgb(255, Current, Current, Current);
+
+                        if (Max < Current) Max = Current;
+
+                        bm.SetPixel(x, y, nc);
+                        bm.SetPixel(x + 1, y, nc);
+                        bm.SetPixel(x, y + 1, nc);
+                        bm.SetPixel(x + 1, y + 1, nc);
+                    }
+
+                    if (Normalize)
+                    {
+                        if (Max < 255)
+                        {
+                            float Ratio = 255 / Max;
+
+                            for (x = 0; x < 512; x++)
+                                for (y = 0; y < 512; y++)
+                                {
+                                    Color c = bm.GetPixel(x, y);
+                                    int Normalized = (int)(c.R * Ratio) % 256;
+                                    Color nc = Color.FromArgb(255, Normalized, Normalized, Normalized);
+                                    bm.SetPixel(x, y, nc);
+                                }
+                        }
+                    }                
+                } else {
+                    Color nc = Color.FromArgb(255, 200, 200, 200);
+                    y = m_data[0] * 2;
+
+                    for (int i = 1; i < m_data.Length; i++)
+                    {
+                        x = y;
+                        y = m_data[i] * 2;
+                        
+                        bm.SetPixel(x, y, nc);
+                        bm.SetPixel(x + 1, y, nc);
+                        bm.SetPixel(x, y + 1, nc);
+                        bm.SetPixel(x + 1, y + 1, nc);
+                    }
+                }
+
+            }
+
+            pictureBox1.Image = bm;
+        }
+        */
+
+        private void OpenBtn_Click(object sender, EventArgs e)
+        {
+            if (openFileDlg.ShowDialog() == DialogResult.OK)
+            {
+                if (m_data != null) m_data = null;
+                
+                FileStream fs = File.OpenRead(openFileDlg.FileName);
+                m_data = new byte[fs.Length];
+                fs.Read( m_data, 0, m_data.Length);
+                fs.Close();
+
+                //DrawData();
+                //CalculateFingerPrint();
+                CalculateRangeFingerPrint(0, m_block_size);
+                DrawFingerPrint();
+                DrawDataImage();
+            }
+        }
+
+        private void Scale_CheckedChanged(object sender, EventArgs e)
+        {
+            //DrawData();
+            DrawFingerPrint();
+        }
+
+        private void NormalizeChk_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ScaleChk.Checked) {
+                //DrawData();
+                DrawFingerPrint();
+            }
+        }
+
+        private void DataPictureBox_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DataPictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (m_data != null)
+            {
+                if (m_previous_y != e.Y)
+                {
+                    if (pictureBox1.Height > 1)
+                    {
+                        int position = (int)(((float)e.Y / (float)pictureBox1.Height) * (float)m_data.Length);
+                        int start = position - m_block_size / 2;
+                        int end = position + m_block_size / 2;
+
+                        CalculateRangeFingerPrint(start, end);
+                        DrawFingerPrint();
+                        //DrawDataImage();
+                        m_previous_y = e.Y;
+                    }
+                }
+            }
+            
+        }
+
+        private void DataPictureBox_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DataPictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (m_data != null)
+            {
+                if (m_previous_y != e.Y)
+                {
+                    if (pictureBox1.Height > 1)
+                    {
+                        int position = (int)(((float)e.Y / (float)pictureBox1.Height) * (float)m_data.Length);
+                        int start = position - m_block_size / 2;
+                        int end = position + m_block_size / 2;
+
+                        CalculateRangeFingerPrint(start, end);
+                        DrawFingerPrint();
+                        //DrawDataImage();
+                        m_previous_y = e.Y;
+                    }
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (BlockSizeBox.Text.Length > 0)
+            {
+                try {
+                    m_block_size = Convert.ToInt32(BlockSizeBox.Text);
+                } 
+                catch (Exception ee)
+                {
+                    m_block_size = 10240;
+                }
+            }
+        }
+
+        private void BlockSizeBox_Leave(object sender, EventArgs e)
+        {
+            BlockSizeBox.Text = m_block_size.ToString();
+        }
+    }
+}
